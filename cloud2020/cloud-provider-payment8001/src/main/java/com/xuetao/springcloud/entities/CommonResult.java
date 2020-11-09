e40d52495b999c86916e99af67cd9caf214c474f
litao <2078217489@qq.com>
2020-11-09 19:48:29 +0800

    1

---
+++ cloud2020/cloud-provider-payment8001/src/main/java/com/xuetao/springcloud/entities/CommonResult.java
@@ -0,0 +1,26 @@
+package com.xuetao.springcloud.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * @ClassName CommonResult
+ * @Description TODO
+ * @Author EDZ
+ * @DaTe 2020/11/9 19:19
+ * @Version 1.0
+ **/
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class CommonResult<T> {
+
+    private Integer code;
+    private String message;
+    private T data;
+
+    public CommonResult(Integer code, String message) {
+        this(code, message ,null);
+    }
+}
