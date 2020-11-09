1a4bcaeaee3d9995b8dd160894f8f3f601ecee05
litao <2078217489@qq.com>
2020-11-09 16:33:57 +0800

    测试

---
+++ cloud2020/cloud-provider-payment8001/src/main/java/com/xuetao/springcloud/PaymentMain8001.java
@@ -0,0 +1,17 @@
+package com.xuetao.springcloud;
+
+import org.springframework.boot.SpringApplication;
+
+/**
+ * @ClassName PaymentMain8001
+ * @Description 主启动类
+ * @Author 李涛
+ * @DaTe 2020/11/9 16:11
+ * @Version 1.0
+ **/
+public class PaymentMain8001 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentMain8001.class, args);
+    }
+}
