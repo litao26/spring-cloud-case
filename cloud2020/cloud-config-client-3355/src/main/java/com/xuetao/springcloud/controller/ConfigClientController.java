1e273d34f681c5c900c3f24fcf9d00eb788facd8
litao <2078217489@qq.com>
2020-12-11 17:38:14 +0800

    1

---
+++ cloud2020/cloud-config-client-3355/src/main/java/com/xuetao/springcloud/controller/ConfigClientController.java
@@ -0,0 +1,27 @@
+package com.xuetao.springcloud.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @ClassName ConfigClientController
+ * @Description TODO
+ * @Author EDZ
+ * @DaTe 2020/12/11 17:34
+ * @Version 1.0
+ **/
+@RestController
+@RefreshScope
+public class ConfigClientController
+{
+    @Value("${config.info}")
+    private String configInfo;
+
+    @GetMapping("/configInfo")
+    public String getConfigInfo()
+    {
+        return configInfo;
+    }
+}
